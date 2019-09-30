9737c52ce80f4fac62ecf8ea1e480901afde3081
BY <byagihas@gmail.com>
2019-09-30 17:47:58 -0500

    20190930 - Added .csv write functionality and request promise

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const request = require('request')
+const rp = require('request-promise')
 const cheerio = require('cheerio')
+const fs = require('fs')
 
 let dayMood = ""
 let gainers = []
@@ -7,7 +9,7 @@ let losers = []
 
 let fcrypto = {}
 
-request('https://coinmarketcap.com/', function(err, resp, html) {
+rp('https://coinmarketcap.com/', function(err, resp, html) {
     if (!err){
       const $ = cheerio.load(html)
       let nameHolder = {};
@@ -66,22 +68,26 @@ request('https://coinmarketcap.com/', function(err, resp, html) {
         
         // Set up separate lists with keys for modular use
         Object.keys(fname)
-          .forEach(key => nameHolder[key] = fname[key]);
+          .forEach(key => nameHolder[key] = fname[key])
 
         Object.keys(fpercentage)
-          .forEach(key => percentageHolder[key] = fpercentage[key]);
+          .forEach(key => percentageHolder[key] = fpercentage[key])
         
         Object.keys(fprice)
-          .forEach(key => priceHolder[key] = fprice[key]);
+          .forEach(key => priceHolder[key] = fprice[key])
           
         Object.keys(fmarketcap)
-        .forEach(key => marketCapHolder[key] = fmarketcap[key]);
+        .forEach(key => marketCapHolder[key] = fmarketcap[key])
         
         Object.keys(fdataccid)
-        .forEach(key => dataccIdHolder[key] = fdataccid[key]);
+        .forEach(key => dataccIdHolder[key] = fdataccid[key])
         
         Object.keys(fdataccslug)
-        .forEach(key => dataccSlugHolder[key] = fdataccslug[key]);
+        .forEach(key => dataccSlugHolder[key] = fdataccslug[key])
+
+        // Push headers into losers/gainers array
+        losers.push("SYMBOL,PERCENTAGE")
+        gainers.push("SYMBOL,PERCENTAGE")
 
         // Load all objects into a Gainer or Loser array for example 
         for(i=0;i<=fdataccid.length-1;i++){
@@ -98,15 +104,16 @@ request('https://coinmarketcap.com/', function(err, resp, html) {
 
           Object.keys(fcrypto)
           .forEach(key => cryptoHolder[key] = fcrypto[key]);
-          
+
           // Push lists for example
-          if(fcrypto.percentage <= -5){
-            losers.push(fcrypto.symbol)
+          if(fcrypto.percentage <= -3){
+            
+            losers.push("\n" + fcrypto.symbol + "," + fcrypto.percentage)
           }
-          else if(fcrypto.percentage >= 5){
-            gainers.push(fcrypto.symbol)
-          }
-          
+          else if(fcrypto.percentage >= 3){
+            
+            gainers.push("\n" + fcrypto.symbol + "," + fcrypto.percentage)
+          }          
         }
 
         // Display Gainers and Losers first depending on if Bull or Bear mood, set dayMood var for other purposes.
@@ -125,4 +132,22 @@ request('https://coinmarketcap.com/', function(err, resp, html) {
            
       });
     }
+}).then(function(){
+  // Write CSV with gainers and losers
+  fs.writeFile('gainers.csv', gainers, 'utf8', function (err) {
+    if (err) {
+      console.log('Some error occured - file either not saved or corrupted file saved.');
+    } else{
+      console.log('Gainers saved');
+    }
+  });
+
+  fs.writeFile('losers.csv', losers, 'utf8', function (err) {
+    if (err) {
+      console.log('Some error occured - file either not saved or corrupted file saved.');
+    } else{
+      console.log('Losers saved');
+    }
+  });
+
 });
